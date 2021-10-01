54643db1419017e3d056dc2d1052bda1da4e79c2
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2021-10-01 21:05:02 +0900

    fix: 프론트엔드 폴더 디렉토리 수정, 파비콘 및 타이틀 수정, 전역 폰트 설정

---
+++ frontend/src/Components/Router.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.scss";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+const AppRouter = () => {
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+    // <Router>
+    //   <Switch>
+    //     <Route exact path="/" component={Home} />
+    //     <Route exact path="/mypage" component={Mypage} />
+    //     <Route exact path="/introduction" component={Introduction} />
+    //     <Route exact path="/report" component={Report} />
+    //     <Route exact path="/test" component={Test} />
+    //     <Route exact path="/result" component={Result} />
+    //     <Route exact path="/watchvideo" component={WatchVideo} />
+    //   </Switch>
+    // </Router>
+  );
+};
+
+export default AppRouter;
