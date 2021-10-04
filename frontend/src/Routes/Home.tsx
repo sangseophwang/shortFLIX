c86186c00a12e8b80312249e273c36035806f486
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2021-10-04 17:54:12 +0900

    feat: Home 페이지 디자인 구현
    Former-commit-id: ca0752d94965ad3b83b55fa98d8b77b9aedcb9c1

--- frontend/src/Routes/Home.tsx
+++ frontend/src/Routes/Home.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import MainVideo from "../Components/Home/MainVideo";
+import Summary from "../Components/Home/Summary";
+import Bottom from "../Components/Home/Summary_Bottom";
+import Footer from "../Components/Common/Footer";
 import "./scss/Home.scss";
 
 const Home = () => {
   return (
-    <div id="HomeContainer">
+    <div className="HomeContainer">
       <MainVideo />
+      <Summary />
+      <Bottom />
+      <Footer />
     </div>
   );
 };
