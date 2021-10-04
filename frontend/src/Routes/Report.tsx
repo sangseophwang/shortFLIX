b09b50070e8783b97dcce0fd9f9850f7702874e4
[2기-2반] 이금홍 <guemhong3780@gmail.com>
2021-10-04 11:18:43 +0000

    Revert "Merge branch 'feat-backend-db' into 'master'"
    This reverts merge request !5
    
    Former-commit-id: 02fdd8f4dcfd8ad08c7e55fe170461602e3b92bc

--- frontend/src/Routes/Report.tsx
+++ frontend/src/Routes/Report.tsx
@@ -1,11 +1,1 @@
-import React from "react";
-
-const Report = () => {
-  return (
-    <div>
-      <h1>Report</h1>
-    </div>
-  );
-};
-
-export default Report;
+export {};
--- frontend/src/Routes/Result.tsx
+++ frontend/src/Routes/Result.tsx
@@ -1,11 +1,1 @@
-import React from "react";
-
-const Result = () => {
-  return (
-    <div>
-      <h1>Result</h1>
-    </div>
-  );
-};
-
-export default Result;
+export {};
