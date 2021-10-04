bfde58c661d56d62e1287bf0241f5498562a48ce
[2기-1반] 최연주 <jujubee6323@gmail.com>
2021-10-04 20:19:23 +0900

    fix: Home, MainVideo 원래대로 돌림
    Former-commit-id: 21c3d647158838d5d82c74886aa7018307c7f490

--- frontend/src/Components/Home/MainVideo.tsx
+++ frontend/src/Components/Home/MainVideo.tsx
@@ -2,15 +2,15 @@ import React from "react";
 import "./scss/MainVideo.scss";
 import background from "../Common/Video/background.mp4";
 import Logo from "../Common/Image/shortFLIX.png";
-import Button from "../Common/Button";
 
 export default function MainVideo() {
+  const history = useHistory();
   return (
     <div>
       <div className="title">
         <img src={Logo} alt="shortFLIX LOGO" />
         <h1>,for your busy life</h1>
-        <Button styles='btn-sm btn-red'>Get Started</Button>
+        <button>Get Started</button>
       </div>
 
       <video muted autoPlay loop playsInline>
