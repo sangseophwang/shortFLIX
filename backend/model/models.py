88d17f5905fb28da8be7c713cfecc301a81c0fa0
[2기-2반] 이금홍 <guemhong3780@gmail.com>
2021-10-06 08:19:28 +0900

    fix : 트리 구조 수정
    Former-commit-id: 74fa1f61474b3dfb2fb72675c44c13b1c3cc40e1

--- backend/model/models.py
+++ backend/model/models.py
@@ -8,13 +8,13 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(30), unique=True, nullable=False)
     user_name = db.Column(db.String(30), nullable=False)
-    preferences = db.Column(db.String(30), nullable=True)
+    preferences = db.Column(db.JSON, nullable=True)
     # 배열 형태로 insert해야함
-    # https://zionh.tistory.com/49
-    
     likes_list = db.Column(db.JSON, nullable=True)
     
     # __init__을 쓰면 다른 곳에서 인스턴스에 인자값을 넘길 수 있다
-    def __init__(self, email, user_name):
+    def __init__(self, email, user_name, preferences, likes_list):
         self.email = email
         self.user_name = user_name
+        self.preferences = preferences
+        self.likes_list = likes_list
