2b36008ac494d94fcea958d0f3cbfe17a431e809
[2기-2반] 이금홍 <guemhong3780@gmail.com>
2021-10-06 09:40:06 +0900

    refactor : db관련 코드 수정
    Former-commit-id: a45aca6526d8af9eaa1dbbe42511c6fc3068df8e

--- backend/view/__init__.py
+++ backend/view/__init__.py
@@ -3,21 +3,21 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from authlib.integrations.flask_client import OAuth
 
-def create_app():
+db = SQLAlchemy()
 
+def create_app():
   #Flask 객체 인스턴스 생성
   app = Flask(__name__)
   app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:root@localhost:3306/reviews_db" 
   app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
   app.config['SECRET_KEY'] = "dev1234"
-  
-  db = SQLAlchemy(app)
+
   db.init_app(app)
   migrate = Migrate(app, db)
   migrate.init_app(app, db)
+  
+  from model import models
 
- 
-  # oauth = OAuth(app)
   from view import login_route
   app.register_blueprint(login_route.bp)
   
