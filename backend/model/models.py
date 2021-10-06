e50c517e967eceedce3fff5110287ee3a6019499
[2기-2반] 이금홍 <guemhong3780@gmail.com>
2021-10-07 00:39:24 +0900

    feat : sql파일 추가 & 드라마 영화 테이블 추가
    Former-commit-id: 2fa9eada4045d081784a2a2c146b8b62fe9698dd

--- backend/model/models.py
+++ backend/model/models.py
@@ -14,9 +14,17 @@ class User(db.Model):
     latest_reviews_list = db.Column(db.JSON, nullable=True)
     
     # __init__을 쓰면 다른 곳에서 인스턴스에 인자값을 넘길 수 있다
-    def __init__(self, email, user_name, preferences, likes_list, latest_reviews_list):
+    def __init__(self, email, user_name):
         self.email = email
         self.user_name = user_name
-        self.preferences = preferences
-        self.likes_list = likes_list
-        self.likes_list = latest_reviews_list
+
+class Content(db.Model):
+    __tablename__ = 'media_content'
+    id = db.Column(db.Integer, primary_key=True)
+    thumbnail = db.Column(db.Text(), nullable=False)
+    title = db.Column(db.Text(), nullable=False)
+    rating = db.Column(db.Float(), nullable=False)
+    genre = db.Column(db.Text(), nullable=True)
+    running_time = db.Column(db.Text(), nullable=True)
+    synop = db.Column(db.Text(), nullable=True)
+
