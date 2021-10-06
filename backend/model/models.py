1901963d34f7f356499abbff9039d0cf65d3acf6
[2기-2반] 이금홍 <guemhong3780@gmail.com>
2021-10-06 09:40:06 +0900

    refactor : db관련 코드 수정
    Former-commit-id: 264f768ca7852b8979b5ce6de5d68184fd703226

--- backend/model/models.py
+++ backend/model/models.py
@@ -1,5 +1,5 @@
 # DB 관련 코드
-from app import db
+from view import db
 
 class User(db.Model):
     
@@ -11,10 +11,12 @@ class User(db.Model):
     preferences = db.Column(db.JSON, nullable=True)
     # 배열 형태로 insert해야함
     likes_list = db.Column(db.JSON, nullable=True)
+    latest_reviews_list = db.Column(db.JSON, nullable=True)
     
     # __init__을 쓰면 다른 곳에서 인스턴스에 인자값을 넘길 수 있다
-    def __init__(self, email, user_name, preferences, likes_list):
+    def __init__(self, email, user_name, preferences, likes_list, latest_reviews_list):
         self.email = email
         self.user_name = user_name
         self.preferences = preferences
         self.likes_list = likes_list
+        self.likes_list = latest_reviews_list
