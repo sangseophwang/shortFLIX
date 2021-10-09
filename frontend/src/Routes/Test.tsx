e5c0194a74ecec13f2ae1aaa0861f1658fe5a128
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2021-10-10 01:45:56 +0900

    fix: 코드 수정
    Former-commit-id: 06c3d64267cb4932356600d8d981ddfa57fdfabd

--- frontend/src/Routes/Test.tsx
+++ frontend/src/Routes/Test.tsx
@@ -1,31 +1,29 @@
 import React, { useState, useEffect } from "react";
 import SelectButton from "../Components/Test/SelectButton";
-import TestChoice from '../Components/Test/TestChoice';
-import GenreChoice from '../Components/Test/GenreChoice';
+import TestChoice from "../Components/Test/TestChoice";
+import GenreChoice from "../Components/Test/GenreChoice";
 import SearchChoice from "../Components/Test/SearchChoice";
 import Loading from "../Components/Test/Loading";
 
-
-
 type TestProps = {
   match: any;
-}
+};
 
-type Page = '' | 'like' | 'dislike' | 'search' | 'randchoice' | 'loading' ; 
+type Page = "" | "like" | "dislike" | "search" | "randchoice" | "loading";
 
-const Test = ({match}: TestProps) => {
-  const [page, setPage] = useState<Page>('')
+const Test = ({ match }: TestProps) => {
+  const [page, setPage] = useState<Page>("");
   useEffect(() => {
-    setPage(match.params.detail || '')
-  }, [])
-  console.log(page)
+    setPage(match.params.detail || "");
+  }, []);
+  console.log(page);
   return (
-    <main id='Test__Container'>    
-      {page === '' && <TestChoice isRandom={false} />}
-      {page === 'like' && <GenreChoice isLike={true} />}
-      {page === 'dislike' && <GenreChoice isLike={false} />}
-      {page === 'search' && <SearchChoice />}
-      {page === 'randchoice' && <TestChoice isRandom={true}/>}
+    <main id="Test__Container">
+      {page === "" && <TestChoice isRandom={false} />}
+      {page === "like" && <GenreChoice isLike={true} />}
+      {page === "dislike" && <GenreChoice isLike={false} />}
+      {page === "search" && <SearchChoice />}
+      {page === "randchoice" && <TestChoice isRandom={true} />}
     </main>
   );
 };
