625b479deedacedee8dac489850e99e4b61419fd
[2기-2반] 이금홍 <guemhong3780@gmail.com>
2021-10-11 00:59:10 +0900

    fix : login_route.py 디폴트 / 라우트 삭제
    Former-commit-id: fb59975f9ea80864ba3c5e2f8523563f462866fd

--- backend/view/login_route.py
+++ backend/view/login_route.py
@@ -1,15 +1,7 @@
-from flask import render_template, Blueprint
-
-# from view import create_app
+from flask import Blueprint
 from service.google_login import GoogleLogin
 
 bp = Blueprint('google',__name__, url_prefix='/')
-# app = create_app()
-
-# Default route
-@bp.route('/')
-def index():
-  return render_template('login.html')
 
 # Google login route
 @bp.route('/login/google')
