e3b3da6b0f5dc2ca4e6b631792104ef4d895b96f
[2기-2반] 이금홍 <guemhong3780@gmail.com>
2021-10-10 22:11:02 +0900

    fix : 코드 리뷰 사항 반영
    Former-commit-id: 00aba9510069cdde3d12e7873164fba02dd60c4b

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,8 @@
-from flask import Flask, render_template, session, request, url_for, redirect, flash, jsonify, Blueprint
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from authlib.integrations.flask_client import OAuth
 from view import create_app
-from flask_cors import CORS
 
 
 app = create_app()
-CORS(app)
+
 
 if __name__=="__main__":
   app.run(host="0.0.0.0", port=80, debug=True)
