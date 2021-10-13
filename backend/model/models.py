0f9ed032ef672e9c80da9118731ff13f9ad7b949
[2기-2반] 이금홍 <guemhong3780@gmail.com>
2021-10-13 15:45:25 +0900

    fix : 회원 가입 기능 수정
    Former-commit-id: a6bf7d2c1af803fae0d1d6f2d74bca9b5b6d8a15

--- backend/model/models.py
+++ backend/model/models.py
@@ -8,15 +8,17 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(30), unique=True, nullable=False)
     user_name = db.Column(db.String(30), nullable=False)
+    pwd = db.Column(db.String(30), nullable=True)
     preferences = db.Column(db.JSON, nullable=True)
     # 배열 형태로 insert해야함
     likes_list = db.Column(db.JSON, nullable=True)
     latest_reviews_list = db.Column(db.JSON, nullable=True)
     
     # __init__을 쓰면 다른 곳에서 인스턴스에 인자값을 넘길 수 있다
-    def __init__(self, email, user_name):
+    def __init__(self, email, user_name, pwd):
         self.email = email
         self.user_name = user_name
+        self.pwd = pwd
 
 class Content(db.Model):
     __tablename__ = 'media_content'
