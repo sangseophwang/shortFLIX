b54d7e77e876ccc67d66e2a2e10fde7143a04e50
[2기-1반]한동희 <dongheehan4@gmail.com>
2021-10-15 00:46:02 +0900

    fix: 실수 수정
    Former-commit-id: 84f6f15be00211d6c3ebaf4bca74a1731acd4e8f

--- backend/route/filter_route.py
+++ backend/route/filter_route.py
@@ -5,9 +5,8 @@ from service.filter_contents import filter_content
 bp = Blueprint('filter',__name__)
 
 # Default route
-@bp.route('/filter', methods=['GET'])
+@bp.route('/filter', methods=['POST'])
 def filters():
-  # request_data = json.loads(request.data)
-  request_data = {}
-  request_data['email'] = 'man@gmail.com'
+  request_data = json.loads(request.data)
+
   return filter_content(request_data)
