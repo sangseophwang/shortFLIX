f25ef18c8a9cabc68dabf8c4f735d6b30b8b0484
[2기-1반]한동희 <dongheehan4@gmail.com>
2021-10-15 00:40:50 +0900

    fix: 컨텐츠 추천 기능 수정/ 32개로 불러옴
    Former-commit-id: 7b9c125be82739a3e11a99e4c18b4b33b3e777a6

--- backend/route/filter_route.py
+++ backend/route/filter_route.py
@@ -5,8 +5,9 @@ from service.filter_contents import filter_content
 bp = Blueprint('filter',__name__)
 
 # Default route
-@bp.route('/filter', methods=['POST'])
+@bp.route('/filter', methods=['GET'])
 def filters():
-  request_data = json.loads(request.data)
-
+  # request_data = json.loads(request.data)
+  request_data = {}
+  request_data['email'] = 'man@gmail.com'
   return filter_content(request_data)
