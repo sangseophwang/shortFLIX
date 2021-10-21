a1410df68e58cd92e88185ab127f6139f6878e92
최연주 <jujubee6323@gmail.com>
2021-10-21 17:49:26 +0900

    fix: hover로 퍼센티지 나오는 효과 추가
    Former-commit-id: 91336e8136ae007f232188dd6da919c8dcb82690

--- frontend/src/Components/Result/MovieItem.tsx
+++ frontend/src/Components/Result/MovieItem.tsx
@@ -1,21 +1,26 @@
-import React from 'react'
-import './scss/MovieItem.scss'
-import {useHistory} from 'react-router'
+import React from "react";
+import "./scss/MovieItem.scss";
 
 type MovieITemProps = {
-    image?: string;
-    title: string;
-    onClick: any;
-}
+  image?: string;
+  title: string;
+  onClick: any;
+  percent?: any;
+};
 
-export default function MovieItem( { image, title, onClick }:MovieITemProps) {
-    const history = useHistory()
-    return (
-        <div className='movieItem' onClick={onClick}>
-            <img src={image} />
-            <div className='movieTitle'>
-                {title}
-            </div>
-        </div>
-    )
+export default function MovieItem({
+  image,
+  title,
+  onClick,
+  percent,
+}: MovieITemProps) {
+  const finalPercent = Math.round(percent * 100);
+
+  return (
+    <div className="movieItem" onClick={onClick}>
+      <img src={image} />
+      <div className="movieTitle">{title}</div>
+      <div className="percentage">{finalPercent}% 일치</div>
+    </div>
+  );
 }
